ece15d54662d62cfbba250f042024804dc059462
trekhleb <3000285+trekhleb@users.noreply.github.com>
2026-03-22 10:00:49 -0700

    [skip-e2e] Add transit redirect

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./globals.css";
 import Home from "./app/page";
+import { resolveVanityRedirect } from "./utils/vanity-redirect";
+
+const redirect = resolveVanityRedirect(window.location.pathname, import.meta.env.BASE_URL);
+if (redirect) {
+  window.history.replaceState(null, "", redirect);
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
